c9da024fbd17a65065fe52444d5ca7a4de1702e5
Michael Manhire <michaelmanhire@gmail.com>
2022-06-04 21:31:57 -0700

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 function App() {
   return (
-    <main>
-      <h1>Try Pronouns</h1>
-      <section>
-        <h2>Choose Your Pronouns</h2>
+    <main className="app">
+      <h1 className="visually-hidden">Try Pronouns</h1>
+      <section className="form-container">
+        <h2 className="visually-hidden">Choose Your Pronouns</h2>
         <form>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input id="name" name="name" type="text" />
+          <div className="form-control">
+            <label className="form-label" htmlFor="name">Name</label>
+            <input className="form-input" id="name" name="name" type="text" placeholder="Name" />
           </div>
-          <fieldset>
-            <legend>Pronouns</legend>
-            <div>
-              <label htmlFor="subjective">Subjective pronoun</label>
-              <input id="subjective" name="subjective" type="text" />
-              <label htmlFor="objective">Objective pronoun</label>
-              <input id="objective" name="objective" type="text" />
+          <fieldset className="form-control form-fieldset">
+            <legend className="form-label">Pronouns</legend>
+            <div className="form-input-group">
+              <label className="visually-hidden" htmlFor="subjective">Subjective pronoun</label>
+              <input className="form-input" id="subjective" name="subjective" type="text" placeholder="she, he, they, etc." />
+              <span aria-hidden="true">/</span>
+              <label className="visually-hidden" htmlFor="objective">Objective pronoun</label>
+              <input className="form-input" id="objective" name="objective" type="text" placeholder="her, him, them, etc." />
             </div>
           </fieldset>
-          <button type="submit">Try It Out</button>
+          <div className="form-button-wrapper">
+            <button className="button" type="submit">Try It Out</button>
+          </div>
         </form>
       </section>
-      <section>
-        <h2>Sample Text</h2>
-        <p>After you submit the form with your name and pronouns, some generated text will appear below.</p>
+      <section className="text-container">
+        <h2 className="visually-hidden">Sample Text</h2>
+        <p className="guidance">After entering your name and pronouns, some sample text will appear below:</p>
         <hr />
         <div />
       </section>
